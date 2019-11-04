a905bc7ede5141d891022102edb2ba65568521f6
Stefan Lulham <stefan.lulham@gmail.com>
2019-11-04 15:03:03 +0000

    Showing a simple window with glfw

--- game/src/main/c/game/game.c
+++ game/src/main/c/game/game.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+#include <GLFW/glfw3.h>
+
 int main() {
-    printf("Hello, World!\n");
+    if (glfwInit()) {
+        GLFWwindow* window = glfwCreateWindow(1600, 900, "Project Areobiz", NULL, NULL);
+
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
 }
